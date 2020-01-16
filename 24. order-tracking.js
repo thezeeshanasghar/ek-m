fc4ed8021e99f2dcb0b612df1e9dad3538ebc0cd
therazaashraf <therazaashraf1988@gmail.com>
2020-01-16 16:38:09 +0500

    Order Tracking Bar Animation

--- 24. order-tracking.js
+++ 24. order-tracking.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     loadOrderStatus(OrderId);
     $(".order-con-1 .green-round").css('height' , '18px');
     function barDown(){
-      $(".order-con-1 .green-bar").css('height' , '57px');  
+      $(".order-con-1 .green-bar").css('height' , '100%');  
    };
    window.setTimeout( barDown, 1700 ); // 3 seconds
    window.setInterval("loadOrderStatus(OrderId)", 10000);
@@ -20,32 +20,51 @@ function loadOrderStatus (id)
             if (result ==1)
             {
                 
-            setTimeout('$(".order-con-2 .green-bar").css("height" , "57px");', 3000);
-            // $(".order-con-2 .green-bar").css('height' , '57px');
+            setTimeout('$(".order-con-2 .green-bar").css("height" , "100%");', 3000);
 
             setTimeout('$(".order-con-2 .green-round").css("height" , "18px");', 4300);
-            // $(".order-con-2 .green-round").css('height' , '18px');
 
-            setTimeout('$(".order-con-2 .green-bar2").css("height" , "57px");', 5800);
-            // $(".order-con-2 .green-bar2").css('height' , '57px');
+            setTimeout('$(".order-con-2 .green-bar2").css("height" , "100%");', 5800);
             }
             else if (result ==2)
             {
 
-                // setTimeout('$(".order-con-2 .green-bar").css("height" , "57px");', 7000);
-        
-                
-                $(".order-con-2 .green-round").css('height' , '18px');
-                $(".order-con-2 .green-bar2").css('height' , '57px');
-                $(".order-con-3 .green-bar").css('height' , '57px');
-                $(".order-con-3 .green-round").css('height' , '18px');
+
+                setTimeout('$(".order-con-2 .green-bar").css("height" , "100%");', 3000);
+
+                setTimeout('$(".order-con-2 .green-round").css("height" , "18px");', 4300);
+
+                setTimeout('$(".order-con-2 .green-bar2").css("height" , "100%");', 5800);
+
+
+
+                setTimeout('$(".order-con-3 .green-bar").css("height" , "100%");', 7800);
+
+                setTimeout('$(".order-con-3 .green-round").css("height" , "18px");', 9600);
+
                 $("#orderTrackBtn").attr("onclick" ,"window.location.href='25. track-location.html'");
                 $("#orderTrackBtn").css("background-color", "#039611");
+
+                
             }
             else if (result ==3)
             {
-                $(".order-con-3 .green-bar").css('height' , '57px');
-                $(".order-con-3 .green-round").css('height' , '18px');
+
+                setTimeout('$(".order-con-2 .green-bar").css("height" , "100%");', 3000);
+
+                setTimeout('$(".order-con-2 .green-round").css("height" , "18px");', 4300);
+
+                setTimeout('$(".order-con-2 .green-bar2").css("height" , "100%");', 5800);
+
+
+
+                setTimeout('$(".order-con-3 .green-bar").css("height" , "100%");', 6800);
+
+                setTimeout('$(".order-con-3 .green-round").css("height" , "18px");', 9600);
+
+                $("#orderTrackBtn").attr("onclick" ,"window.location.href='25. track-location.html'");
+                $("#orderTrackBtn").css("background-color", "#039611");
+
             }
         },
         error: function(xhr, status, error) {
