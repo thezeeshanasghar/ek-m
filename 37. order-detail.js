5a29d2a9726024e36b6b6882159debe8fd6b176e
Naeem_Abbas <naeemabbasa25@gmail.com>
2020-03-16 13:44:20 +0500

    Changes

--- 37. order-detail.js
+++ 37. order-detail.js
@@ -22,6 +22,8 @@
                     success: function (result) {
                        var html = '' ;
                        var summary = '';
+                       console.log(result);
+                       
                        $.each(result.OrderItems, function (index, item)
                        {
                       html+= ' <li><div class="left-panel">'+ item.Name+'</div><div class="mid-panel">'+item.Quantity+'</div>';
@@ -34,10 +36,55 @@
                       $("#DelCharges").html(result.Fee);
                       $("#GST").html(result.GST);
                       $("#Total").html(result.GrandTotal);
-                       
+                      result.OrderStatus !=3?$("#Reorder").css("display","none"):$("#Reorder").css("display","block");
+
                     },
                     error: function(xhr, status, error) {
                         console.log(xhr.responseText);
                       }
                 });
             }
+            function ReOrder(){
+              $.ajax({
+                  url: SERVER + "order/" +OrderId,
+                  type: "GET",
+                  dataType: "JSON",
+                  contentType: "application/json;charset=utf-8",
+                  success: function (result) {
+                      localStorage.setItem('grandTotal' , result.GrandTotal);
+                      localStorage.setItem('PayMethod' , result.PayMethod);
+                      console.log(result);
+                      var order = {
+                          Subtotal: result.Subtotal,
+                          GrandTotal: result.GrandTotal,
+                          Fee: result.Fee,
+                          GST: result.GST,
+                          Address: result.Address,
+                          PayMethod: result.PayMethod,
+                          Instruction:result.Instruction,
+                          OrderItems : result.OrderItems,
+                          CustomerId: result.CustomerId,
+                          CityId : result.CityId,
+                          RestaurantId: result.RestaurantId 
+                      }
+                      $.ajax({
+                          url: SERVER + "order/customer-order",
+                          type: "POST",
+                          data: JSON.stringify(order),
+                          dataType: "json",
+                          contentType: "application/json;charset=utf-8",
+                          success: function (result) {
+                                  localStorage.setItem("Id" , result.Id)
+                                  alert("Your order is placed successfully");
+                                  localStorage.removeItem("items");
+                                  window.location.reload(true);
+                                  location.href = '23. order-placed.html';
+                          }
+                      });
+                  },
+                  error: function(xhr, status, error) {
+                      console.log(xhr.responseText);
+                    }
+              });
+          
+          }
